f247c304413a3f8bd0c2bbd41a08c0907fcd8732
Bartosz Bąk <barbak1994@gmail.com>
2020-02-09 15:30:40 +0100

    prototype of day in group calendar

--- src/components/group/group.tsx
+++ src/components/group/group.tsx
@@ -1,13 +1,14 @@
 import React, { ReactElement } from 'react';
 
 import './group.scss';
+import { GroupCalendar } from '../group-calendar';
 
 export function Group(): ReactElement {
   return (
     <div className="c-group">
       <div>Users</div>
       <div>Chat</div>
-      <div>Calendar</div>
+      <GroupCalendar />
     </div>
   );
 }
